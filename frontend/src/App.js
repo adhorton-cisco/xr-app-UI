030076b37ba2d1d7b0fd81e006f91abcfd5f56a0
Eliot Pickhardt <epickhardt@wisc.edu>
2022-08-04 16:07:58 -0500

    frontend changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,11 @@ class App extends Component {
     super(props);
     this.state = {
       name: '',
-      package: null
+      package: null,
+      username: '',
+      password: '',
+      ipaddress: '',
+      port: 0,
       };
   }
 
@@ -28,7 +32,27 @@ class App extends Component {
       let form_data = new FormData();
       form_data.append('name', this.state.name);
       form_data.append('package', this.state.package)
-      let url = 'http://localhost:8000/tester/packages/';
+      let url = 'http://localhost:8000/appmgr/packages/';
+      axios.post(url, form_data, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+          .then(res => {
+            console.log(res.data);
+          })
+          .catch(err => console.log(err.response))
+    };
+
+    handleSubmitForm2 = (e) => {
+      e.preventDefault();
+      console.log(this.state);
+      let form_data = new FormData();
+      form_data.append('username', this.state.username);
+      form_data.append('password', this.state.password)
+      form_data.append('ipaddress', this.state.ipaddress);
+      form_data.append('port', this.state.port)
+      let url = String.concat('http://localhost:8000/appmgr/packages/install/' + this.state.name);
       axios.post(url, form_data, {
         headers: {
           "Content-Type": "multipart/form-data",
@@ -45,14 +69,30 @@ class App extends Component {
         <div className="App">
           <form onSubmit={this.handleSubmit} encType="multipart/form-data">
             <p>
-              <input type="text" placeholder='Name' id='name' value={this.state.name} onChange={this.handleChange} required/>
+              &emsp;<input type="text" placeholder='Name' id='name' value={this.state.name} onChange={this.handleChange} required/>
             </p>
             <p>
-              <input type="file"
-                     id="package"
-                     accept=".py, .rpm" onChange={this.handleFileChange} required/>
+              &emsp;<input type="file"
+                      id="package"
+                      accept=".py, .rpm" onChange={this.handleFileChange} required/>
             </p>
-            <input type="submit"/>
+            &emsp;<input type="submit"/>
+          </form>
+
+          <form onSubmit={this.handleSubmitForm2} encType="multipart/form-data">
+            <p>
+              &emsp;<input type="text" placeholder='Username' id='username' value={this.state.username} onChange={this.handleChange} required/>
+            </p>
+            <p>
+              &emsp;<input type="text" placeholder='Password' id='password' value={this.state.password} onChange={this.handleChange} required/>
+            </p>
+            <p>
+              &emsp;<input type="text" placeholder='IP Address' id='ipadress' value={this.state.ipaddress} onChange={this.handleChange} required/>
+            </p>
+            <p>
+              &emsp;GRPC Port <input type="number" placeholder='GRPC Port' id='port' value={this.state.port} onChange={this.handleChange} required/>
+            </p>
+            &emsp;<input type="submit"/>
           </form>
         </div>
       );
